81e8a001a0a810101c367a537eab6ca70e4012ed
SerenaFraD <dursoserena61@gmail.com>
2021-02-13 14:13:58 +0100

    User Operation, ma con qualche errore e da testare - Serena

--- YourBook/src/model/UserBean.java
+++ YourBook/src/model/UserBean.java
@@ -1,23 +1,28 @@
 package model;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.ArrayList;
 
-public class UserBean implements Serializable, Cloneable {
+public class UserBean extends Bean implements Serializable, Cloneable {
 
     private static final long serialVersionUID = 1L;
 
     private int id_utente;
     private String email;
     private String nome;
+    private String password;
     private String nazionalita;
     private Boolean auth;
 
-    public UserBean(int id_utente, String email, String nome, String nazionalita, Boolean auth) {
-        this.id_utente = id_utente;
-        this.email = email;
-        this.nome = nome;
-        this.nazionalita = nazionalita;
-        this.auth = auth;
+    public UserBean() {
+        id_utente = -1;
+        nome = email = password = nazionalita = "";
+        auth = false;
     }
 
     public int getId_utente() {
@@ -44,6 +49,21 @@ public class UserBean implements Serializable, Cloneable {
         this.nome = nome;
     }
 
+    public void setPasswordhash(String passwordNew) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(passwordNew.getBytes(StandardCharsets.UTF_8));
+            this.password = String.format("%040x", new BigInteger(1, digest.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
     public String getNazionalita() {
         return nazionalita;
     }
@@ -52,12 +72,24 @@ public class UserBean implements Serializable, Cloneable {
         this.nazionalita = nazionalita;
     }
 
-    public Boolean getAuth() {
+    //also known as -> getAuth -> ritorna true se l'utente è un admin, false altrimenti
+    public Boolean isAdmin() {
         return auth;
     }
 
     public void setAuth(Boolean auth) {
         this.auth = auth;
+    }
+
+    @Override
+    public UserBean clone() {
+        UserBean bean = null;
+        try {
+            bean = (UserBean) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return bean;
     }
 
     @Override
@@ -72,10 +104,30 @@ public class UserBean implements Serializable, Cloneable {
     }
 
     @Override
-    public boolean equals(Object otherObj){
-        if(otherObj == null || otherObj.getClass() != getClass())
+    public boolean equals(Object otherObj) {
+        if (otherObj == null || otherObj.getClass() != getClass())
             return false;
         UserBean other = (UserBean) otherObj;
         return other.id_utente == id_utente;
+    }
+
+    @Override
+    public List<String> getKey() {
+        ArrayList<String> keys = new ArrayList<String>();
+        keys.add(email);
+        return keys;
+    }
+
+    @Override
+    public int compareKey(Bean otherBean) {
+        if(this.getClass() != otherBean.getClass())
+            return 1;
+        UserBean other = (UserBean) otherBean;
+        return email.compareTo(other.email);
+    }
+
+    @Override
+    public String getBeanName() {
+        return nome;
     }
 }
